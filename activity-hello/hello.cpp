469599274c947fb1887e691878f54aa912ee4fc8
ashish <ashishauthenticash@gmail.com>
2021-02-05 09:37:56 -0500

    Activity changes VM

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -8,38 +8,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-void checkHostName(int hostname){
-  if(hostname == -1){
-    perror("gethostname");
-    exit(1);}
-}
-
-void checkIPbuffer(char *IPbuffer){
-  if(NULL == IPbuffer){
-    perror("inet_ntoa");
-    exit(1);
-  }}
-void checkHostEntry(struct hostent * hostentry){
-  if(hostentry == NULL){
-    perror("gethostbyname");
-    exit(1);}}
-
 int main () {
 
-  char hostbuffer[256];
-  char *IPbuffer;
+  char hostName[256];
+  char *IPAddress;
   struct hostent *host_entry;
   int hostname;
-  hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-  checkHostName(hostname);
+  hostname = gethostname(hostName, sizeof(hostName));
 
-  host_entry = gethostbyname(hostbuffer);
-  checkHostEntry(host_entry);
+  host_entry = gethostbyname(hostName);
 
-  IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
+  IPAddress = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
 
-  printf("Hostname: %s\n", hostbuffer);
-  printf("Host IP: %s", IPbuffer);
+  printf("Hostname: %s\n", hostName);
+  printf("Host IP: %s\n", IPAddress);
 
   return 0;
 }
